75c442de51987c683317284c4ad1421870af98ef
dmptmkn <dm.ptmkn@yandex.ru>
2024-06-08 22:35:10 +0300

    add Contact class implementation

--- src/main/java/com/example/contacts/model/Contact.java
+++ src/main/java/com/example/contacts/model/Contact.java
@@ -1,4 +1,28 @@
 package com.example.contacts.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "contacts")
 public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
 }
