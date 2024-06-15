94f9a34012fa564c556928bf7c1fb10a887480be
dmptmkn <dm.ptmkn@yandex.ru>
2024-06-15 23:21:44 +0300

    add basic database service logging

--- src/main/java/com/example/contacts/model/service/impl/ContactServiceImpl.java
+++ src/main/java/com/example/contacts/model/service/impl/ContactServiceImpl.java
@@ -4,10 +4,12 @@ import com.example.contacts.model.Contact;
 import com.example.contacts.model.repository.ContactRepository;
 import com.example.contacts.model.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ContactServiceImpl implements ContactService {
@@ -18,7 +20,12 @@ public class ContactServiceImpl implements ContactService {
     public void create(Contact contact) {
         if (contact == null) throw new IllegalArgumentException();
 
-        contactRepository.save(contact);
+        try {
+            contactRepository.save(contact);
+            log.info("New contact created: " + contact);
+        } catch (Exception e) {
+            log.error("Error while creating new contact: " + e.getMessage());
+        }
     }
 
     @Override
@@ -37,15 +44,25 @@ public class ContactServiceImpl implements ContactService {
     public void update(Long id, String phoneNumber) {
         if (id == null || phoneNumber == null) throw new IllegalArgumentException();
 
-        Contact contactToUpdate = findById(id);
-        contactToUpdate.setPhoneNumber(phoneNumber);
-        contactRepository.save(contactToUpdate);
+        try {
+            Contact contactToUpdate = findById(id);
+            contactToUpdate.setPhoneNumber(phoneNumber);
+            contactRepository.save(contactToUpdate);
+            log.info("Contact updated: " + contactToUpdate);
+        } catch (Exception e) {
+            log.error("Error while updating a contact: " + e.getMessage());
+        }
     }
 
     @Override
     public void delete(Long id) {
         if (id == null) throw new IllegalArgumentException();
 
-        contactRepository.deleteById(id);
+        try {
+            contactRepository.deleteById(id);
+            log.info("Contact with id " + id + "have been deleted");
+        } catch (Exception e) {
+            log.error("Error while deleting a contact: " + e.getMessage());
+        }
     }
 }
